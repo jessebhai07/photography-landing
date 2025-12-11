fcb110f516f2497fce14860dcbe181cdcb2f8745
rimu-7 <rrimu99@gmail.com>
2025-12-11 23:51:40 +0800

    update image configuration to use remote patterns and add Vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.pexels.com", "googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
